9de8d9979f4c77451cc1ebf6d1c3aabac56011fb
Kyle Crawford <kcrwfrd@gmail.com>
2014-02-10 00:13:00 -0500

    Refactor cmd2launchd to LaunchdPlist class

---
+++ lib/launchd_tools/launchd_plist.rb
@@ -0,0 +1,43 @@
+require 'rexml/document'
+include REXML
+module LaunchdTools
+  class LaunchdPlist
+    attr_reader :doc, :program_args_array_element
+
+    def initialize
+      base = %q[<?xml version='1.0' encoding='UTF-8'?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">]
+      @doc = Document.new(base)
+      plist_element = Element.new('plist')
+      doc.add_element(plist_element, {"version" => "1.0"})
+      base_dictionary = Element.new('dict')
+      plist_element.add_element(base_dictionary)
+      program_args_key_element = Element.new('key')
+      program_args_key_element.text = 'ProgramArguments'
+      base_dictionary.add_element(program_args_key_element)
+      @program_args_array_element = Element.new "array"
+      base_dictionary.add_element(program_args_array_element)
+    end
+
+
+    def add_program_arg(arg)
+      string_element = Element.new "string"
+      string_element.text = arg
+      program_args_array_element << string_element
+    end
+
+    def add_program_args(args)
+      args.each do |arg|
+        add_program_arg(arg)
+      end
+    end
+
+    def to_stdout
+      formatter = REXML::Formatters::Pretty.new # (2)
+      formatter.compact = true
+      formatter.write(doc, $stdout)
+      puts
+    end
+  end
+end
+
